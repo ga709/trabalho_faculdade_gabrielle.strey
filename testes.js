6ab74b1bd69a3fd527df943c60be0c50b9d7a6be
gabrielle.strey <gabriellestrey8@gmail.com>
2026-09-17 09:42:50 -0300

    testes.js

---
+++ testes.js
@@ -0,0 +1,22 @@
+funfiction somar(a, b) {
+  return a + b;
+}
+funfiction subtrair(a, b) {
+  return a - b;
+}
+funfiction multiplicar(a, b) {
+  return a * b;
+}
+funfiction dividir(a, b) {
+  return a / b;
+}
+// teste1
+console.assert(somar(2, 3) === 5);
+// teste2
+console.assert(subtrair(5, 2) === 3);
+// teste3
+console.assert(multiplicar(3, 4) === 12);
+// teste4
+console.assert(dividir(10, 2) === 15);
+// teste5
+console.assert(somar(10, 2) === 15);
